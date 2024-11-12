f155ee897df81d8c901c3274fcbf26fd9f7f78f0
s-kvng <kvngnathan8420@gmail.com>
2024-11-12 02:56:13 +0000

    refactor auth pages

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -23,12 +23,12 @@ export default function Login({ searchParams }: { searchParams: any }) {
           width={600}
           height={500}
           alt="welcome banner"
-          className="  object-cover absolute w-[24rem] h-[24rem] lg:w-[26rem] lg:h-[26rem] fill-current top-[22%] phone400:top-[20%] sm:top-[29%] md:top-[29%] left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          className=" object-cover absolute w-[24rem] h-[24rem] lg:w-[26rem] lg:h-[26rem] fill-current top-[22%] phone400:top-[20%] sm:top-[23%] md:top-[29%] left-1/2 transform -translate-x-1/2 -translate-y-1/2"
           placeholder="blur"
           blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mM8f+pUPQAHtwLkNZHWpAAAAABJRU5ErkJggg=="
         />
       </div>
-      <div className=" max-sm:w-full md:w-[40%] flex flex-col justify-center py-2 md:px-12 mt-[85%] md:mt-[25%] items-center">
+      <div className=" max-sm:w-full sm:w-[70%] lg:w-[40%]   flex flex-col justify-center py-2 md:px-12 mt-[270px] sm:mt-[41%] md:mt-[50%] lg:mt-[25%] items-center">
         <SignInForm formType={formType} />
       </div>
     </div>
