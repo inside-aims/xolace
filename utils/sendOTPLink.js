97f0137ab6ae6e7cff1e552981f89f42f2f8b0ce
s-kvng <kvngnathan8420@gmail.com>
2024-12-06 09:37:32 +0000

    Update sendOTPLink.js

--- utils/sendOTPLink.js
+++ utils/sendOTPLink.js
@@ -56,7 +56,7 @@ export async function sendOTPLink(email, type, request) {
   }
 
   await transporter.sendMail({
-    from: "Xolace <no-reply@xolace.app>",
+    from: "Xolace <no-reply@mail.xolace.app>",
     to: email,
     subject: mailSubject,
     html: `
