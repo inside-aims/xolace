f60079f59aa3ee9309ffeccf42dcce7df27b0f55
s-kvng <kvngnathan8420@gmail.com>
2024-12-26 09:38:17 +0000

    Create TagCard.tsx

---
+++ components/cards/TagCard.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import React from "react";
+
+import { Badge } from "../ui/badge";
+import { Tag } from "lucide-react";
+
+interface TagCardProps {
+  _id: string;
+  name: string;
+  posts?: number;
+  showCount?: boolean;
+  compact?: boolean;
+}
+
+const TagCard = ({ _id, name, posts, showCount }: TagCardProps) => {
+  return (
+    <Link href={`/tag/${_id}`} className=" flex justify-between gap-2">
+      <Badge variant="outline">
+        <Tag className="w-3 h-3 mr-1" />
+        {name}
+      </Badge>
+
+      {showCount && (
+        <p className="small-medium text-dark500_light700">{posts}</p>
+      )}
+    </Link>
+  );
+};
+
+export default TagCard;
