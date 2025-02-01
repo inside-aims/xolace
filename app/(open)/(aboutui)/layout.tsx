8d64ce2517c798197805b44a5cf81cb5d4f21e89
s-kvng <kvngnathan8420@gmail.com>
2025-02-01 20:51:34 +0000

    fix light theme for about 🔦

--- app/(open)/(aboutui)/layout.tsx
+++ app/(open)/(aboutui)/layout.tsx
@@ -6,11 +6,11 @@ export default async function AboutUiLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="flex min-h-screen flex-row bg-black">
+    <main className="flex min-h-screen flex-row bg-white dark:bg-black">
       <div className="hidden md:block fixed top-0 h-screen overflow-y-auto">
         <AboutLeftSidebar/>
       </div>
-      <div className="flex-1 overflow-y-auto md:pl-[110px] lg:pl-[240px]">
+      <div className="flex-1 overflow-y-auto md:pl-[110px] lg:pl-[240px] bg-white dark:bg-black">
         {children}
       </div>
     </main>
