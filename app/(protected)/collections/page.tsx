e73e74b4e8012cffd14a6e6535da557c371a9922
s-kvng <kvngnathan8420@gmail.com>
2025-03-18 00:19:54 +0000

    fix layout on some pages 👍

--- app/(protected)/collections/page.tsx
+++ app/(protected)/collections/page.tsx
@@ -5,7 +5,7 @@ import FeedSkeletonLoader from '@/components/shared/loaders/FeedSkeletonLoader'
 const CollectionPage = () => {
   return (
     <div className="md:container md:mx-auto py-6">
-      <h1 className="text-2xl font-bold mb-6">Your Collections 📚 </h1>
+      <h1 className="text-4xl font-bold mb-6">Your Collections 📚 </h1>
       <div className="space-y-4">
         <Suspense fallback={<FeedSkeletonLoader />}>
           <Collections />
