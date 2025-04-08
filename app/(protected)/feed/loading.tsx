2863360626df02f1403a82332d7286f462aa8cc8
s-kvng <kvngnathan8420@gmail.com>
2025-04-08 05:10:14 +0000

    add loading fallback for feed

---
+++ app/(protected)/feed/loading.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import FeedSkeletonLoader from '@/components/shared/loaders/FeedSkeletonLoader';
+
+const Loading = () => <FeedSkeletonLoader/>;
+
+export default Loading;
