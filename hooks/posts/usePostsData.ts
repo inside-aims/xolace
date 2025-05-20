60c4f7c08db67be135a82b9989ee4b97cc380814
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-05-20 22:56:15 +0000

    add react query package 📦

---
+++ hooks/posts/usePostsData.ts
@@ -0,0 +1,38 @@
+import { useQuery } from '@tanstack/react-query';
+import { getSupabaseBrowserClient } from '@/utils/supabase/client';
+
+const supabase = getSupabaseBrowserClient();
+
+export function usePosts() {
+  return useQuery({
+    queryKey: ['posts'],
+    queryFn: async () => {
+      const { data, error } = await supabase
+      .from('posts')
+      .select(
+        `
+         *,
+         posttags (
+            tags (
+              name
+            )
+          ),
+            votes(
+            user_id,
+            vote_type
+            ),
+            comments:comments(count),
+            views:views(count),
+          collections(
+            user_id
+          )  
+      `,
+      )
+      .order('created_at', { ascending: false });
+      
+      if (error) throw error;
+      return data;
+    },
+    staleTime: 5 * 60 * 1000,
+  });
+}
