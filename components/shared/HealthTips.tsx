03d6e274ff17972fb7f0494daf345669dc3111a7
Emmanuel <emmsom506@gmail.com>
2025-05-21 00:56:21 +0000

    Mobile fix up

--- components/shared/HealthTips.tsx
+++ components/shared/HealthTips.tsx
@@ -7,7 +7,7 @@ interface HealthTipCardProps {
   title: string;
   author: string;
   content: string;
-  date: string; // ISO format
+  date: string;
   index?: number;
   total?: number
 }
@@ -34,31 +34,43 @@ const healthTips: HealthTipCardProps[] = [
     content: 'Journaling is a tool for self-reflection and emotional processing. Learn how to start a mindful journal...',
     date: '2025-10-17',
   },
-  {
-    id: '4',
-    title: 'Hydration and Mental Clarity',
-    author: 'Dr. Henry Osei',
-    content: 'Dehydration can affect mood and focus. Discover the importance of regular water intake...',
-    date: '2025-08-15',
-  },
+  // {
+  //   id: '4',
+  //   title: 'Hydration and Mental Clarity',
+  //   author: 'Dr. Henry Osei',
+  //   content: 'Dehydration can affect mood and focus. Discover the importance of regular water intake...',
+  //   date: '2025-08-15',
+  // },
 ];
 
 export default function HealthTips() {
   return (
     <div className="flex flex-col w-full md:px-4 gap-4">
-      <div className="hidden md:flex flex-col gap-2  p-2 md:p-4 items-start border shadow-lg rounded-2xl">
-        <h4 className="w-full flex justify-between items-center">
+      <div className="flex flex-col gap-2  py-2 md:py-4 items-start border shadow-lg rounded-xl">
+        <h4 className="w-full flex justify-between items-center  px-2 md:px-4 ">
           <span className={"font-semibold"}>
             Xolace Wellness Insights
           </span>
-          <Cross size={22}/>
+          <Cross size={18}/>
         </h4>
-        <div>
-          Practical tips and trusted advice to help you thrive—mind and body.
+        <div className={"flex flex-col items-center gap-2"}>
+          <p className={"flex ps-2 md:px-4 "}>
+            Practical health tips and trusted advice to help you thrive mind and body.
+          </p>
+          <p className={"flex ms-2 md:ms-4  border-t py-1 md:py-2"}>
+            Backed by experts, tailored for your everyday wellbeing.
+          </p>
+          <div className={"w-full py-2 flex items-center justify-center md:hidden border-t"}>
+            <Button
+              className={"w-full mx-8 bg-lavender-400 hover:bg-lavender-500 rounded-full transition-transform duration-300 ease-in-out hover:scale-110"}
+            >
+              View Health Tips
+            </Button>
+          </div>
         </div>
       </div>
 
-      <div className="flex flex-col gap-2 md:gap-4 py-2 md:py-4 items-start border shadow-lg rounded-2xl">
+      <div className="hidden md:flex flex-col gap-2 md:gap-4 py-2 md:py-4 items-start border shadow-lg rounded-xl">
         <h4 className="w-full flex justify-between items-center px-2 md:px-4">
           <span className={"font-semibold"}>Health Tips</span>
           <Info size={22}/>
@@ -68,7 +80,7 @@ export default function HealthTips() {
           {healthTips.map((healthTip, index) => (
             <div
               key={healthTip.id}
-              className={`${index > 1 ? 'hidden md:block ' : 'block'}`}
+              className={`${index > 0 ? 'hidden md:block ' : 'block'}`}
             >
               <HealthTipCard
                 id={healthTip.id}
