b801ff6797c28028c7c58287ae8f3a1473410f34
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-05-23 00:09:52 +0000

    fix help ui alittle bit 😅

--- components/shared/layoutUIs/HealthTipsWrapper.tsx
+++ components/shared/layoutUIs/HealthTipsWrapper.tsx
@@ -7,12 +7,12 @@ desktop device. It takes children component
  */
 export default function HealthTipsWrapper({children}: {children: ReactNode})  {
   return (
-    <div className={"w-full grid grid-cols-12 md:h-screen"}>
+    <div className={"w-full grid grid-cols-12 md:h-[calc(100vh-var(--header-height))]"}>
       <div
         className="px-0! pt-8 pb-12 sm:container overflow-auto col-span-12 md:col-span-8">
         {children}
       </div>
-      <div className={"border-s mt-1 col-span-12 md:col-span-4 hidden md:flex h-screen overflow-auto"}>
+      <div className={"border-s pt-1 col-span-12 md:col-span-4 hidden md:flex h-full overflow-auto"}>
        <HealthTips/>
       </div>
     </div>
