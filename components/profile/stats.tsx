329d2cbd178e53a03a67026ac3b7e6d5f5bcdfa0
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-05-28 20:06:24 +0000

    Update stats.tsx

--- components/profile/stats.tsx
+++ components/profile/stats.tsx
@@ -13,10 +13,10 @@ export function Stats({reputation, userId}: StatsProps) {
   const { data , isPending } = useAssignBadges(userId);
 
   //Dummy profile stats for badges
-  const profileStatBadges: {key: string, value: number, name: string, imageURL: string}[] = [
-    { key: "gold", value: data?.GOLD || 0, name: "Gold Badges", imageURL: "gold-medal" },
-    { key: "silver", value: data?.SILVER || 0, name: "Silver Badges", imageURL: "silver-medal" },
-    { key: "bronze", value: data?.BRONZE || 0, name: "Bronze Badges", imageURL: "bronze-medal" },
+  const profileStatBadges: {key: string, value: number, name: string, imageURL: string, className?: string}[] = [
+    { key: "gold", value: data?.GOLD || 0, name: "Gold Badges", imageURL: "gold-medal", className: "border-honey-600 hover:border-honey-800" },
+    { key: "silver", value: data?.SILVER || 0, name: "Silver Badges", imageURL: "silver-medal", className: "border-zinc-400 hover:border-zinc-600" },
+    { key: "bronze", value: data?.BRONZE || 0, name: "Bronze Badges", imageURL: "bronze-medal", className: "border-yellow-800 hover:border-yellow-800/50" },
   ]
 
   return(
@@ -39,7 +39,7 @@ export function Stats({reputation, userId}: StatsProps) {
           {profileStatBadges.map((badge) => (
             <div
               key={badge.key}
-              className="col-span-12 md:col-span-3 p-4 shadow-lg rounded-lg flex flex-row md:flex-col gap-4 md:gap-8 border h-full"
+              className={`col-span-12 md:col-span-3 p-4 shadow-lg rounded-lg flex flex-row md:flex-col gap-4 md:gap-8 border h-full ${badge.className} `}
             >
               <p className="flex w-8 h-8 flex-col gap-0 m-0">
                 <Image
