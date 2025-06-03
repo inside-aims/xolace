a71e43f518060c7d2c79e94896040c24f4f9a125
Emmanuel <emmsom506@gmail.com>
2025-06-03 10:48:33 +0000

    Comment out the footer section

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -9,22 +9,22 @@ export default async function Layout({
   return (
     <main className="relative overflow-y-auto overflow-x-hidden">
       {children}
-      <footer className="mx-auto flex w-full flex-col items-center justify-center border-t h-30 text-center text-xs dark:bg-bg-dark">
-        <div className="flex items-center justify-center gap-8">
-          <p>
-            Powered by{' '}
-            <Link
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              AIMS
-            </Link>
-          </p>
-          <ThemeSwitcher />
-        </div>
-      </footer>
+      {/*<footer className="mx-auto flex w-full flex-col items-center justify-center border-t h-30 text-center text-xs dark:bg-bg-dark">*/}
+      {/*  <div className="flex items-center justify-center gap-8">*/}
+      {/*    <p>*/}
+      {/*      Powered by{' '}*/}
+      {/*      <Link*/}
+      {/*        href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"*/}
+      {/*        target="_blank"*/}
+      {/*        className="font-bold hover:underline"*/}
+      {/*        rel="noreferrer"*/}
+      {/*      >*/}
+      {/*        AIMS*/}
+      {/*      </Link>*/}
+      {/*    </p>*/}
+      {/*    <ThemeSwitcher />*/}
+      {/*  </div>*/}
+      {/*</footer>*/}
     </main>
   );
 }
