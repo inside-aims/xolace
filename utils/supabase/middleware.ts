64e5809e0f0ef2c8ab017af3b9ee930c9ba00cf9
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-07-26 08:21:31 +0000

    Update middleware.ts

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -28,12 +28,12 @@ export const updateSession = async (request: NextRequest) => {
             return request.cookies.getAll();
           },
           setAll(cookiesToSet) {
-            cookiesToSet.forEach(({ name, value }) =>
-              request.cookies.set(name, value),
-            );
-            response = NextResponse.next({
-              request,
-            });
+            // cookiesToSet.forEach(({ name, value }) =>
+            //   request.cookies.set(name, value),
+            // );
+            // response = NextResponse.next({
+            //   request,
+            // });
             cookiesToSet.forEach(({ name, value, options }) =>{
               response.cookies.set(name, value, options)
             });
