02f8d56ba26f84ae4199ad63251a812d58c8af5d
Emmanuel <emmsom506@gmail.com>
2025-08-03 03:05:44 +0000

    Add verifying state for otp

---
+++ components/professionals/states/VerifyingState.tsx
@@ -0,0 +1,149 @@
+"use client";
+
+import React, {useEffect, useRef, useState} from "react";
+import {OnboardingState} from "@/components/professionals/onboarding";
+import {Controller, useForm} from "react-hook-form";
+import {InputOTP, InputOTPGroup, InputOTPSlot} from "@/components/ui/input-otp";
+import { cn } from "@/lib/utils";
+import {Button} from "@/components/ui/button";
+
+interface VerifyingStateProps {
+  setState: (state: OnboardingState) => void;
+}
+
+interface OTPForm{
+  otp_code: string;
+}
+
+export const VerifyingState = ({setState}: VerifyingStateProps ) => {
+ const [otpUuid, setOtpUuid] = useState<string>('');
+  const [timeLeft, setTimeLeft] = useState(120);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset
+  } = useForm<OTPForm>({
+    defaultValues: { otp_code: "" },
+  });
+
+  useEffect(() => {
+    const otp_uuid = localStorage.getItem("otp_uuid");
+    if(!otp_uuid) {
+      setState("starting");
+    } else {
+      setOtpUuid(otp_uuid)
+    }
+    startTimer();
+
+    return () => {
+      if(timerRef.current) {
+        clearInterval(timerRef.current)
+      }
+    }
+  }, [setState]);
+
+  const startTimer = () => {
+    if (timerRef.current) {
+      clearInterval(timerRef.current);
+    }
+
+    timerRef.current = setInterval(() => {
+      setTimeLeft((prev) => {
+        if (prev <= 1) {
+          if (timerRef.current) {
+            clearInterval(timerRef.current);
+            timerRef.current = null;
+          }
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+  };
+
+  const handleResend = () => {
+    setTimeLeft(120);
+    startTimer();
+    return () => {
+      if(timerRef.current) {
+        clearInterval(timerRef.current)
+      }
+    }
+  };
+
+  const onFormSubmit = (data: OTPForm) => {
+    const payload: {otp_uuid: string, otp_code: string} = {
+      otp_uuid: otpUuid,
+      otp_code: data.otp_code
+    }
+    console.log("data", payload);
+    setState("finished"); //after success
+    reset();
+  };
+
+  return (
+    <div
+      className="relative flex flex-col bg-white rounded-2xl w-full max-w-5xl h-[70vh] md:h-[60vh] overflow-hidden items-center justify-center p-8 gap-6 text-center dark:bg-gray-600 text-foreground ">
+
+      <div className="flex flex-col items-center justify-center gap-4 border p-8">
+        <div className={"flex flex-col items-center gap-4"}>
+          <h2 className={"font-semibold text-2xl"}>Verify Your Account</h2>
+          <p>Enter the OTP sent to your email to verify your account.</p>
+        </div>
+        <form onSubmit={handleSubmit(onFormSubmit)} className="flex flex-col gap-4">
+          <Controller
+            name="otp_code"
+            control={control}
+            rules={{
+              required: "OTP is required",
+              minLength: {value: 6, message: "OTP must be 6 digits"},
+              maxLength: {value: 6, message: "OTP must be 6 digits"}
+            }}
+            render={({field}) => (
+              <InputOTP maxLength={6} className="w-full" {...field}>
+                <InputOTPGroup className="w-full flex justify-between gap-2 md:gap-4">
+                  {[0, 1, 2, 3, 4, 5].map(index => (
+                    <InputOTPSlot
+                      key={index}
+                      index={index}
+                      className="flex-1 h-12 w-12 min-w-0 border border-input"
+                    />
+                  ))}
+                </InputOTPGroup>
+              </InputOTP>
+            )}
+          />
+
+          {errors.otp_code && (
+            <p className="text-red-500 text-sm">{errors.otp_code.message}</p>
+          )}
+
+          <p className={cn(
+            "text-sm font-medium flex justify-end",
+            timeLeft <= 10 ? "text-red-500" : "text-green-600"
+          )}>
+            Time Remaining: {timeLeft}s
+          </p>
+
+          {timeLeft <= 0 ? (
+            <Button
+              type="button"
+              onClick={handleResend}
+              className="w-full bg-ocean-500 dark:bg-ocean-500 text-white font-semibold hover:text-white rounded-lg hover:bg-ocean-600 transition duration-300 ease-in-out hover:bg-ocean-600 dark:text-white hover:scale-102"
+            >Resend OTP</Button>
+          ) : (
+            <Button
+              type="submit"
+              className="w-full bg-lavender-500 dark:bg-lavender-500 text-white font-semibold hover:text-white rounded-lg hover:bg-lavender-600 transition duration-300 ease-in-out hover:bg-lavender-600 dark:text-white hover:scale-102"
+              disabled={watch("otp_code").length < 6}
+            >Submit OTP</Button>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+};
