87c624dd6fe1ef16814e90fe23fd64dad5c59406
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-08-04 09:14:55 +0000

    Update ProceedingState.tsx

--- components/professionals/states/ProceedingState.tsx
+++ components/professionals/states/ProceedingState.tsx
@@ -1,31 +1,35 @@
-import Image from "next/image";
-import React from "react";
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 export const ProceedingState = () => {
   return (
-    <div
-      className="relative flex flex-col bg-white rounded-2xl w-full max-w-5xl h-[70vh] md:h-[60vh] overflow-hidden items-center justify-center p-8 gap-6 text-center dark:bg-gray-600 text-foreground ">
+    <div className="text-foreground relative flex h-[70vh] w-full max-w-5xl flex-col items-center justify-center gap-6 overflow-hidden rounded-2xl bg-white p-8 text-center md:h-[60vh] dark:bg-gray-600">
       {/* Glowing image + message */}
       <div className="flex flex-col items-center justify-center gap-4">
         <div className="animate-bounce-scale">
-          <Image
+          {/* <Image
             src={"/assets/images/mas.webp"}
             alt={"Xolace logo"}
             width={160}
             height={160}
             className="w-48 h-48"
+          /> */}
+          <DotLottieReact
+            src="https://lottie.host/8586490e-8c75-47c6-afc2-c8f2a6f10682/hYjxZaYm6a.lottie"
+            loop
+            autoplay
+            style={{ width: '160px', height: '160px' }}
           />
         </div>
-        <span className="text-lg text-neutral-500 font-medium tracking-wide dark:text-neutral-300">
-         {"Thanks for joining, your seat by the fire is almost ready"}
-          <span className="inline-block animate-dots ml-1">.</span>
+        <span className="text-lg font-medium tracking-wide text-neutral-500 dark:text-neutral-300">
+          {'Thanks for joining, your seat by the fire is almost ready'}
+          <span className="animate-dots ml-1 inline-block">.</span>
         </span>
       </div>
 
-      <div className="absolute top-0 inset-x-1 h-1 bg-white overflow-hidden">
-        <div
-          className="absolute w-[200%] h-full bg-gradient-to-r from-lavender-400 via-lavender-500 to-lavender-400 animate-slide"/>
+      <div className="absolute inset-x-1 top-0 h-1 overflow-hidden bg-white">
+        <div className="from-lavender-400 via-lavender-500 to-lavender-400 animate-slide absolute h-full w-[200%] bg-gradient-to-r" />
       </div>
     </div>
-  )
-}
+  );
+};
