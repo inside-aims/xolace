6c9a22afa7bc0f062e2f5b3d0149c40524acd36f
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-08-04 21:04:13 +0000

    Update inviteError.tsx

--- components/professionals/inviteError.tsx
+++ components/professionals/inviteError.tsx
@@ -1,13 +1,22 @@
-import Alert, {ErrorAlert} from "../shared/xolace-alert"
-import { Button } from "../ui/button"
+import Link from 'next/link';
+import { ErrorAlert } from '../shared/xolace-alert';
+import { Button } from '../ui/button';
 
 const InviteError = () => {
-    return (
-        <div className="w-full flex flex-col items-center justify-center dark:bg-bg-dark bg-bg text-foreground min-h-screen gap-3 md:px-0 px-2">
-            <ErrorAlert title="Invite Error" description="Invite code is invalid or expired. If this is a mistake, please contact support." supportingText="xolace25@gmail.com"/>
-            <Button className="bg-lavender-500 hover:bg-lavender-600 text-white">Back to login</Button>  
-        </div>
-    )
-}
+  return (
+    <div className="dark:bg-bg-dark bg-bg text-foreground flex min-h-screen w-full flex-col items-center justify-center gap-3 px-2 md:px-0">
+      <ErrorAlert
+        title="Invite Error"
+        description="Invite code is invalid or expired. If this is a mistake, please contact support."
+        supportingText="xolace25@gmail.com"
+      />
+      <Link href="/sign-in" replace>
+        <Button className="bg-lavender-500 hover:bg-lavender-600 text-white">
+          Back to login
+        </Button>
+      </Link>
+    </div>
+  );
+};
 
-export default InviteError
+export default InviteError;
