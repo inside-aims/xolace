673cf55b69d784781e6abeca61351a9f3584bec6
Emmanuel <emmsom506@gmail.com>
2025-08-12 21:50:19 +0000

    Chore campfire list on mobile

--- components/campfires/campfires-list.tsx
+++ components/campfires/campfires-list.tsx
@@ -4,7 +4,7 @@ import { AlertCircle, RefreshCw, Search } from 'lucide-react';
 import { Input } from '@/components/ui/input';
 import React, { useCallback, useState } from 'react';
 import { CampfirePurpose } from '@/components/campfires/campfires.types';
-import CampfireCard from '@/components/campfires/campfire-card';
+import {CampfireDesktopCard, CampfireMobileCard} from '@/components/campfires/campfire-cards';
 import { FilterByPurpose } from '@/components/campfires/filtered-purpose';
 import CampfireWrapper from '@/components/shared/layoutUIs/CampfireWrapper';
 import { useAllPublicCampfires } from '@/queries/campfires/getAllPublicCampfires';
@@ -13,14 +13,17 @@ import { useUserState } from '@/lib/store/user';
 import CampfiresListSkeleton from '@/components/campfires/campfires-list-skeleton';
 import { Alert, AlertDescription } from '@/components/ui/alert';
 import { Button } from '@/components/ui/button';
+import {Separator} from "@/components/ui/separator";
 
-const CampfiresList = () => {
+interface CampfireListProps {
+  isDiscover?: boolean;
+}
+const CampfiresList = ({isDiscover = true}: CampfireListProps) => {
   const user = useUserState(state => state.user);
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [selectedPurposes, setSelectedPurposes] = useState<CampfirePurpose[]>(
     [],
   );
-
 
   const {
     data: campfires,
@@ -59,14 +62,19 @@ const CampfiresList = () => {
       <div className="flex w-full flex-col items-center gap-4 px-4">
         {/* Page Heading */}
         <div className="space-y-2 text-center">
-          <h1 className="text-2xl font-bold">Discover Campfires</h1>
+          <h1 className="text-2xl font-bold">
+            {`${isDiscover ? "Discover" : "Manage"} Campfires`}
+          </h1>
           <p className="text-muted-foreground">
-            Find your circle. Join discussions that matter to you.
+            {isDiscover
+              ? "Find your circle. Join discussions that matter to you."
+              : "View and organize the campfires you're already part of."}
           </p>
         </div>
 
         {/* Search & Filter */}
-        <div className="sticky -top-8 z-50 flex w-full max-w-xl items-center justify-between gap-4 py-2 md:justify-center">
+        <div
+          className="sticky -top-8 z-50 flex w-full max-w-xl items-center justify-between gap-4 py-2 md:justify-center">
           <div className="flex w-full items-center rounded-s-2xl border px-2 sm:w-2/3">
             <Search className="text-muted-foreground mr-2 h-5 w-5" />
             <Input
@@ -120,22 +128,45 @@ const CampfiresList = () => {
                 )}
               </div>
           ) : (
-            <div className="grid w-full grid-cols-1 items-stretch gap-6 pt-4 pb-5 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-            {filteredCampfires?.map(campfire => (
-              <CampfireCard
-                key={campfire.campfireId}
-                campfireId={campfire.campfireId}
-                name={campfire.name}
-                description={campfire.description}
-                members={campfire.members}
-                purpose={campfire.purpose}
-                iconURL={campfire.iconURL}
-                slug={campfire.slug}
-                onJoin={() => handleJoinClick(campfire.campfireId)}
-                isMember={campfire.isMember}
-              />
-            ))}
-          </div>
+            <>
+              <div
+                className="grid md:hidden w-full grid-cols-1 items-stretch gap-1 pt-2 pb-5 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+                {filteredCampfires?.map(campfire => (
+                 <>
+                   <Separator/>
+                   <CampfireMobileCard
+                     key={campfire.campfireId}
+                     campfireId={campfire.campfireId}
+                     name={campfire.name}
+                     description={campfire.description}
+                     members={campfire.members}
+                     purpose={campfire.purpose}
+                     iconURL={campfire.iconURL}
+                     slug={campfire.slug}
+                     onJoin={() => handleJoinClick(campfire.campfireId)}
+                     isMember={campfire.isMember}
+                   />
+                 </>
+                ))}
+              </div>
+              <div
+                className="hidden md:grid w-full grid-cols-1 items-stretch gap-6 pt-4 pb-5 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+                {filteredCampfires?.map(campfire => (
+                  <CampfireDesktopCard
+                    key={campfire.campfireId}
+                    campfireId={campfire.campfireId}
+                    name={campfire.name}
+                    description={campfire.description}
+                    members={campfire.members}
+                    purpose={campfire.purpose}
+                    iconURL={campfire.iconURL}
+                    slug={campfire.slug}
+                    onJoin={() => handleJoinClick(campfire.campfireId)}
+                    isMember={campfire.isMember}
+                  />
+                ))}
+              </div>
+            </>
           )}
         </div>
       </div>
