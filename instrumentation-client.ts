71c56cab0020662a6ab2eb6fa3cea1bd430c61d0
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-09-22 17:31:49 +0000

    complete setup

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,45 +1,33 @@
-import * as Sentry from "@sentry/nextjs";
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
 Sentry.init({
-  dsn: "https://1460333a5df80760d32a4efce73e300f@o4510040764973056.ingest.de.sentry.io/4510040773427280",
-  // Adds request headers and IP for users, for more info visit:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
-  //  performance
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for tracing.
-  // We recommend adjusting this value in production
-  // Learn more at
-  // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
-  tracesSampleRate: 1.0,
-  //  performance
-  integrations: [
-    //  session-replay
-    // Replay may only be enabled for the client-side
-    Sentry.replayIntegration(),
-    //  session-replay
-    //  user-feedback
-    Sentry.feedbackIntegration({
-      // Additional SDK configuration goes in here, for example:
-      colorScheme: "system",
-    }),
-    //  user-feedback
-  ],
-  //  session-replay
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  // Learn more at
-  // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-  //  session-replay
-  //  logs
+  dsn: 'https://a147a4bd984ea981b9781551465e5ef8@o4510040764973056.ingest.de.sentry.io/4510063515664464',
+
+  // Add optional integrations for additional features
+  integrations: [Sentry.replayIntegration(),  Sentry.feedbackIntegration({
+    // Additional SDK configuration goes in here, for example:
+    colorScheme: "system",
+  }),],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
   // Enable logs to be sent to Sentry
   enableLogs: true,
-  //  logs
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
+
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
 });
-// This export will instrument router navigations, and is only relevant if you enable tracing.
-// `captureRouterTransitionStart` is available from SDK version 9.12.0 onwards
+
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,23 +1,19 @@
-import * as Sentry from "@sentry/nextjs";
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
 Sentry.init({
-  dsn: "https://1460333a5df80760d32a4efce73e300f@o4510040764973056.ingest.de.sentry.io/4510040773427280",
-  // Adds request headers and IP for users, for more info visit:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
-  //  performance
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for tracing.
-  // We recommend adjusting this value in production
-  // Learn more at
-  // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
-  tracesSampleRate: 1.0,
-  //  performance
-  //  logs
+  dsn: 'https://a147a4bd984ea981b9781551465e5ef8@o4510040764973056.ingest.de.sentry.io/4510063515664464',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
   // Enable logs to be sent to Sentry
   enableLogs: true,
-  //  logs
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
 });
