c6a5c3a792abe0b13f14a15af625b3464bc943d9
Emmanuel <emmsom506@gmail.com>
2025-10-16 13:38:45 +0000

    Add video player to play recording player

--- components/talk-space/mentor/recordings-page.tsx
+++ components/talk-space/mentor/recordings-page.tsx
@@ -3,6 +3,8 @@
 import { Video, Play, ExternalLink } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {CallCard, CallCardProps} from "@/components/talk-space/mentor/call-card";
+import VideoPlayer from "@/components/health-space/reflection/video-player";
+import {useState} from "react";
 
 const recordings: CallCardProps[] = [
   {
@@ -43,42 +45,55 @@ const recordings: CallCardProps[] = [
 ];
 
 const MentorRecordingsPage = () => {
+  const [playVideo, setPlayVideo] = useState<string>("");
+
+  const handlePlayVideo = (recordingId: string) => {
+    setPlayVideo(recordingId);
+  }
+
 
   return (
     <div className="space-y-6">
-      <h1 className="text-lg md:text-3xl font-semibold">Recordings</h1>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        {recordings.map((recording) => (
-          <CallCard
-            key={recording.id}
-            id={recording.id}
-            title={recording.title}
-            date={new Date(recording.date).toLocaleString('en-US', {
-              year: 'numeric',
-              month: 'short',
-              day: '2-digit',
-              hour: '2-digit',
-              minute: '2-digit',
-              hour12: true,
-            })}
-            participants={recording.participants}
-            icon={Video}
-            actions={
-              <>
-                <Button className="bg-lavender-500 hover:bg-lavender-600">
-                  <Play className="w-4 h-4 mr-1" />
-                  Play
-                </Button>
-                <Button variant="outline" className="hover:bg-ocean-600 hover:text-white">
-                  <ExternalLink className="w-4 h-4 mr-1" />
-                  Copy Link
-                </Button>
-              </>
-            }
-          />
-        ))}
-      </div>
+      <h1 className="text-lg md:text-3xl font-semibold">
+        {playVideo ? (`Recording video for ${playVideo}`) : ("Recordings")}
+      </h1>
+      {playVideo ? (
+        <VideoPlayer videoId={playVideo}/>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {recordings.map((recording) => (
+            <CallCard
+              key={recording.id}
+              id={recording.id}
+              title={recording.title}
+              date={new Date(recording.date).toLocaleString('en-US', {
+                year: 'numeric',
+                month: 'short',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: true,
+              })}
+              participants={recording.participants}
+              icon={Video}
+              actions={
+                <>
+                  <Button
+                    className="bg-lavender-500 hover:bg-lavender-600"
+                    onClick={() => handlePlayVideo(recording.id)}>
+                    <Play className="w-4 h-4 mr-1" />
+                    Play
+                  </Button>
+                  <Button variant="outline" className="hover:bg-ocean-600 hover:text-white">
+                    <ExternalLink className="w-4 h-4 mr-1" />
+                    Copy Link
+                  </Button>
+                </>
+              }
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
